760e357356389da1a458ebf98430e14f05a0a5ae
Kenny Rozario <Kenny@Kennys-MacBook-Pro.local>
2015-07-01 00:00:41 -0400

    Third Commit
    Added two live templates and began to clean up the code/make the UI
    look better.

--- app/src/main/java/com/example/kenny/sounddroid/TracksAdapter.java
+++ app/src/main/java/com/example/kenny/sounddroid/TracksAdapter.java
@@ -4,6 +4,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.kenny.sounddroid.com.example.kenny.sounddroid.soundcloud.Track;
@@ -16,10 +17,12 @@ import java.util.List;
 public class TracksAdapter extends RecyclerView.Adapter<TracksAdapter.ViewHolder>{
     public class ViewHolder extends RecyclerView.ViewHolder{
         private final TextView titleTextView;
+        private final ImageView thumbImageView;
 
         ViewHolder(View v){
             super(v);
             titleTextView = (TextView) v.findViewById(R.id.track_title);
+            thumbImageView = (ImageView) v.findViewById(R.id.track_thumbnail);
         }
     }
 
